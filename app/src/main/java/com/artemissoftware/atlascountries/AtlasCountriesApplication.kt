0610fc5338d5d954632746454cca123768b2caea
gustavo <gustavo.isaac@gmail.com>
2025-01-14 15:08:34 +0000

    AtlasCountriesApplication

---
+++ app/src/main/java/com/artemissoftware/atlascountries/AtlasCountriesApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.atlascountries
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AtlasCountriesApplication: Application()
