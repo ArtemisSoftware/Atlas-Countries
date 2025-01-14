4072d8a4fb36d1f9f6955521ac1cad290ab13f5a
gustavo <gustavo.isaac@gmail.com>
2025-01-14 11:48:53 +0000

    Detail

---
+++ app/src/main/java/com/artemissoftware/atlascountries/domain/models/Detail.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.atlascountries.domain.models
+
+data class Detail(
+    val code: String,
+    val name: String,
+    val emoji: String,
+    val capital: String,
+    val currency: String,
+    val languages: List<String>,
+    val continent: String
+)
